f06549718ea1a0c8cf6f2de3da5f60304e938983
Nicola Regattieri <nicregattieri@gmail.com>
2020-10-12 19:02:47 -0300

    mongodb added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom' 
+import { Container } from "react-bootstrap"
+import Header from  './components/Header'
+import Footer from './components/Footer'
+import Homescreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen';
 
 
 function App() {
   return (
-  <>
-    <h1>Welcome the the OneShop</h1>
-  </>
+    <Router>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={Homescreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer/>
+    </Router>
   );
 }
 
 export default App;
+   
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Col, Row } from 'react-bootstrap'
+import Product from '../components/Product'
+
+
+const Homescreen = () => {
+  const [products, setproducts] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('api/products')
+      setproducts(data)
+    }
+    fetchProducts()
+  }, [])
+
+
+
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map(product => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product}/>
+          </Col>
+        ))}
+      </Row>
+    </>
+  )
+}
+
+export default Homescreen
