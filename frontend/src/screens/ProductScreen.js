8ad50ee5035cd762d8197fd3e711fb731fb3ae41
Nicola Regattieri <nicregattieri@gmail.com>
2020-11-02 12:33:03 -0300

    fixed getproduct by id

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -54,7 +54,7 @@ const ProductScreen = ({ history, match }) => {
               </ListGroup.Item>
               <ListGroup.Item>
                 <Rating
-                  value="product.rating"
+                  value={product.rating}
                   text={`${product.numReviews} reviews`}
                 />
               </ListGroup.Item>
