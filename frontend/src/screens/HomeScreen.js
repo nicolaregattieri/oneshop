8da0de112e32ff7440ba1e20157580c1462ca512
Nicola Regattieri <nicregattieri@gmail.com>
2020-11-02 11:28:47 -0300

    home screen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,38 +1,39 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Col, Row } from 'react-bootstrap'
-import Product from '../components/Product'
-import { listProducts } from '../actions/productActions'
-import Loader from '../components/loader'
-import Message from '../components/message'
-
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Col, Row } from 'react-bootstrap';
+import Product from '../components/Product';
+import { listProducts } from '../actions/productActions';
+import Loader from '../components/loader';
+import Message from '../components/Message';
 
 const Homescreen = () => {
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
 
-  const dispatch = useDispatch()
-  const productList =  useSelector(state => state.productList)
-
-  const { loading, error, products } = productList
+  const { loading, error, products } = productList;
 
   useEffect(() => {
-    dispatch(listProducts())
-  }, [dispatch])
-
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <>
       <h1>Latest Products</h1>
-      { loading?  <Loader/>: error ? <Message variant='danger'>{error}</Message> : 
-          <Row>
-            {products.map(product => (
-              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product}/>
-              </Col>
-            ))}
-          </Row>
-      }
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Homescreen
+export default Homescreen;
